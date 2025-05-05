a60b285abf1a37a5bc0317340c23ea8c6882fed5
Denis Svetleishii <denissvetleishii@outlook.com>
2025-05-05 11:20:44 +0400

    fix: fix changeDateFormat

--- arrays-and-strings/changeDateFormat.js
+++ arrays-and-strings/changeDateFormat.js
@@ -1,14 +1,26 @@
 // https://maxcode.dev/problems/change-date-format/
 
 function changeDateFormat(str) {
-    const strArr = str.split('').map((element, index, arr) => {
-        if(element === '/' && typeof Number(arr[index - 1]) === 'number' && typeof Number(arr[index + 1]) === 'number') {
-            element = '.';
+    const parts = str.split(' ');
+    const newParts = parts.map(part => {
+        const clean = part.replace(/[()–]/g, '');
+        const segments = clean.split('/');
+
+        if (segments.length === 3 && 
+            segments[0].length === 2 && 
+            segments[1].length === 2 && 
+            segments[2].length === 4) {
+
+            const newDate = `${segments[1]}.${segments[0]}.${segments[2]}`;
+            return part.replace(clean, newDate);
         }
-        return element;
+
+        return part;
     });
-    return strArr.join('');
+
+    return newParts.join(' ');
 }
+
 
 const str = "World War I or the First World War (07/28/1914 – 11/11/1918) was a global conflict between two coalitions: the Allies and the Central Powers.";
 
