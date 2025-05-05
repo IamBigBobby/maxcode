3d820c981e3368072f250e014422532a532e801a
Denis Svetleishii <denissvetleishii@outlook.com>
2025-05-05 11:30:43 +0400

    feat: add solution for encodeRLE

---
+++ arrays-and-strings/encodeRLE.js
@@ -0,0 +1,25 @@
+// https://maxcode.dev/problems/rle-encode/
+
+function encodeRLE(str) {
+    const strObj = {};
+
+    for (char of str) {
+        if(char in strObj){
+            strObj[char] += 1;
+        } else {
+            strObj[char] = 1;
+        }
+    }
+
+    let resultStr = '';
+
+    for (key in strObj) {
+        resultStr += `${key}${strObj[key] === 1 ? '' : strObj[key]}`;
+    }
+
+    console.log(resultStr);
+    return resultStr;
+}
+
+console.log(encodeRLE("abc")); // "a2b3c5"
+// console.log(encodeRLE("aaaaaaaaaax")); // "a10x"
