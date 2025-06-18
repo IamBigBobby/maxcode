bdf04bd46255b1aeebcb5d9e1f29399c15d5ec96
Denis Svetleishii <denissvetleishii@outlook.com>
2025-06-18 18:08:26 +0400

    feat: add solution for obfuscate

--- arrays-and-strings/emailAddressObfuscator.js
+++ arrays-and-strings/emailAddressObfuscator.js
@@ -1,1 +1,13 @@
 // https://maxcode.dev/problems/email-obfuscator/
+
+function obfuscate(email) {
+    const replaceAt = email.replaceAll('@', ' at ');
+    const replaceDot = replaceAt.replaceAll('.', ' [dot] ');
+    return replaceDot;
+}
+
+console.log(obfuscate('test@123.com'));
+// 'test [at] 123 [dot] com'
+
+console.log(obfuscate('Code_warrior@foo.ac.uk'));
+// 'Code_warrior [at] foo [dot] ac [dot] uk'
